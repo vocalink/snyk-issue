efe1fda7c5b2e1aec71c046ba6ba783dc5927368
Sean Reilly <seanjreilly@gmail.com>
2019-08-12 11:59:54 +0100

    Convert to the simplest possible jar-only example

--- src/main/java/com.example/Application.java
+++ src/main/java/com.example/Application.java
@@ -1,11 +1,7 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.out.println("Hello, World!");
     }
 }
